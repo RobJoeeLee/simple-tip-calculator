ab901c48558c09094c3cbf37f8f38ea106736cd2
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-03-07 16:18:26 -0500

    brought all of my elements from the html file to the js file

---
+++ script.js
@@ -0,0 +1,7 @@
+const calculatorDisplay = document.querySelector(".calculator-display")
+const tipDisplay = document.querySelector(".tip-display")
+const totalWithTipDisplay = document.querySelector(".total-with-tip-display")
+const numberButton = document.querySelectorAll(".number")
+const decimalButton = document.querySelector(".decimal")
+const clearButton = document.querySelector(".clear")
+const tipButton = document.querySelectorAll(".tip-button")
