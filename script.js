5144aa389503e889358147c7766f247745bfc359
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-03-07 16:44:39 -0500

    created the updateDisplay function

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ const tipButton = document.querySelectorAll(".tip-button")
 
 let shouldResetDisplay = false
 
+function updateDisplay(value) {
+    if (shouldResetDisplay){
+        calculatorDisplay.textContent = value
+        shouldResetDisplay = false
+        return
+    }
+    if (calculatorDisplay.textContent.includes(".")){
+        let [whole, decimal] = calculatorDisplay.textContent.split(".")
+        if(decimal.length >= 2)return
+    }
+    calculatorDisplay.textContent = calculatorDisplay.textContent === "0" ? value: calculatorDisplay.textContent + value
+}
