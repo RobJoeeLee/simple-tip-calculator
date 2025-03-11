ae059e7ad1f35be23a81dc0e8adffade224aa4a1
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-03-11 14:09:01 -0400

    added a shouldClearCalculatorDisplay function to reset the calculator and the displays once called

--- script.js
+++ script.js
@@ -7,40 +7,46 @@ const clearButton = document.querySelector(".clear")
 const tipButton = document.querySelectorAll(".tip-button")
 
 let shouldResetDisplay = false
+let shouldClearOnNextInput = false
 
-function updateDisplay(value) {
-    if (shouldResetDisplay){
+function updateCalculatorDisplay(value){
+    if(shouldClearOnNextInput){
         calculatorDisplay.textContent = value
-        shouldResetDisplay = false
+        tipDisplay.textContent = "0"
+        totalWithTipDisplay.textContent = "0"
+        shouldClearOnNextInput = false
         return
     }
-    if (calculatorDisplay.textContent.includes(".")){
+
+    if(calculatorDisplay.textContent.includes(".")){
         let [whole, decimal] = calculatorDisplay.textContent.split(".")
         if(decimal.length >= 2)return
     }
+
     calculatorDisplay.textContent = calculatorDisplay.textContent === "0" ? value: calculatorDisplay.textContent + value
 }
 
 numberButton.forEach(button => {
     button.addEventListener("click" , () => {
-        updateDisplay(button.dataset.number)
+        updateCalculatorDisplay(button.dataset.number)
     })
 })
 
 decimalButton.addEventListener("click" , () => {
+    if(shouldClearOnNextInput){
+        calculatorDisplay.textContent = "0."
+        tipDisplay.textContent = "0"
+        totalWithTipDisplay.textContent = "0"
+        shouldClearOnNextInput = false
+        return
+    }
+
     if(calculatorDisplay.textContent.includes("."))return
     if(calculatorDisplay.textContent === "0"){
         calculatorDisplay.textContent = "0."
         return
     }
     calculatorDisplay.textContent += "."
-})
-
-clearButton.addEventListener("click" , () => {
-    calculatorDisplay.textContent = "0"
-    tipDisplay.textContent = "0"
-    totalWithTipDisplay.textContent = "0"
-
 })
 
 function finalizeDecimal() {
@@ -51,14 +57,22 @@ function finalizeDecimal() {
 
 tipButton.forEach(button => {
     button.addEventListener("click" , () => {
-        billAmount = parseFloat(calculatorDisplay.textContent)
-        if(isNaN(billAmount) || billAmount === 0)return
+        let billAmount = parseFloat(calculatorDisplay.textContent)
+        if(isNaN(billAmount) || billAmount === "0")return
 
         let tipPercentage = parseFloat(button.dataset.percent) / 100
-        let tipAmount = billAmount * tipPercentage
-        let totalWithTip = billAmount + tipAmount
+        let tipAmount = tipPercentage * billAmount
+        let totalWithTip = tipAmount + billAmount
 
         tipDisplay.textContent = tipAmount.toFixed(2)
         totalWithTipDisplay.textContent = totalWithTip.toFixed(2)
+        shouldClearOnNextInput = true
     })
+})
+
+clearButton.addEventListener("click" , () => {
+    calculatorDisplay.textContent = "0"
+    tipDisplay.textContent = "0"
+    totalWithTipDisplay.textContent = "0"
+    shouldClearOnNextInput = false
 })
